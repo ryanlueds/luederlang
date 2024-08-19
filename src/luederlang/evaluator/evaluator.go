824d1c5f5cb6edf3b20c8dce5f85c0a8a4945906
Ryan Lueder <lueder.ryan@gmail.com>
2024-08-18 21:45:58 -0400

    the worst code ive ever produced, evaluating infix operations between different types

--- src/luederlang/evaluator/evaluator.go
+++ src/luederlang/evaluator/evaluator.go
@@ -5,6 +5,38 @@ import (
     "luederlang/ast"
 )
 
+var (
+    TRUE = &object.Boolean{Value: true} // what do you mean these arent const
+    FALSE = &object.Boolean{Value: false}
+    NULL = &object.Null{}
+)
+
+/*var typeToHash = map[object.ObjectType]int8{
+    object.INTEGER_OBJ:1 << 0,
+    object.FLOAT_OBJ: 1 << 1,
+}
+
+func getTypesHash(left, right object.Object) int8 {
+    leftHash, ok := typeToHash[left.Type()]
+    if !ok {
+        // TODO: there's no way i can keep doing this everywhere
+        return 0
+    }
+    rightHash, ok := typeToHash[right.Type()]
+    if !ok {
+        return 0
+    }
+    return leftHash + rightHash
+}
+
+func getValueFromObject[T any](obj object.Object) T {
+    switch obj.Type() {
+    case object.Integer:
+        return obj.(object.Integer
+    
+    }
+}*/
+
 func Eval(node ast.Node) object.Object {
     switch node := node.(type) {
     case *ast.Program:
@@ -15,10 +47,21 @@ func Eval(node ast.Node) object.Object {
         return &object.Integer{Value: node.Value}
     case *ast.FloatLiteral:
         return &object.Float{Value: node.Value}
-
+    case *ast.Boolean:
+        return nativeBoolToBooleanObject(node.Value)
+    case *ast.PrefixExpression:
+        return evalPrefixExpression(node.Operator, Eval(node.Right))
+    case *ast.InfixExpression:
+        return evalInfixExpression(Eval(node.Left), node.Operator, Eval(node.Right))
     }
-    
-    return &object.Null{}
+    return nil
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
 
 func evalStatements(statements []ast.Statement) object.Object {
@@ -30,3 +73,293 @@ func evalStatements(statements []ast.Statement) object.Object {
     
     return result
 }
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+    switch operator {
+    case "!":
+        return evalBangPrefixExpression(right)
+    case "-":
+        return evalMinusPrefixExpression(right)
+    default:
+        return NULL
+    }
+}
+
+func evalBangPrefixExpression(right object.Object) object.Object {
+    switch right {
+    case TRUE:
+        return FALSE
+    case FALSE:
+        return TRUE
+    case NULL:
+        return TRUE
+    default:
+        // TODO: error handling, this should never happen
+        return NULL
+    }
+}
+
+func evalMinusPrefixExpression(right object.Object) object.Object {
+    switch right.Type() {
+    case object.INTEGER_OBJ:
+        return &object.Integer{Value: 0-right.(*object.Integer).Value}
+    case object.FLOAT_OBJ:
+        return &object.Float{Value: 0-right.(*object.Float).Value}
+    default:
+        return NULL
+    }
+}
+
+/*
+ * Left and right sides don't need to be of the same type
+*/
+func evalInfixExpression(
+    left object.Object,
+    operator string,
+    right object.Object,
+) object.Object {
+    switch operator{
+    case "+":
+        return evalPlusInfixExpression(left, right)
+    case "*":
+        return evalMultiplyInfixExpression(left, right)
+    case "-":
+        return evalSubtractInfixExpression(left, right)
+    case "/":
+        return evalDivideInfixExpression(left, right)
+    case ">":
+        return evalGTInfixExpression(left, right)
+    case "<":
+        return evalLTInfixExpression(left, right)
+    case "==":
+        return evalEqualsInfixExpression(left, right)
+    case "!=":
+        return evalNotEqualsInfixExpression(left, right)
+    default:
+        return NULL
+    }
+}
+
+// TODO: Figure out a better way to do this PLEASE
+func evalPlusInfixExpression(left, right object.Object) object.Object {
+    switch left.Type() {
+    case object.INTEGER_OBJ:
+        leftVal := left.(*object.Integer).Value
+        switch right.Type() {
+        case object.INTEGER_OBJ:
+            rightVal := right.(*object.Integer).Value
+            return &object.Integer{Value: leftVal + rightVal}
+        case object.FLOAT_OBJ:
+            rightVal := right.(*object.Float).Value
+            return &object.Float{Value: float64(leftVal) + rightVal}
+        }
+    case object.FLOAT_OBJ:
+        leftVal := left.(*object.Float).Value
+        switch right.Type() {
+        case object.INTEGER_OBJ:
+            rightVal := right.(*object.Integer).Value
+            return &object.Float{Value: leftVal + float64(rightVal)}
+        case object.FLOAT_OBJ:
+            rightVal := right.(*object.Float).Value
+            return &object.Float{Value: leftVal + rightVal}
+        }
+    }
+    // TODO: Figure out error handling
+    return NULL
+}
+
+func evalMultiplyInfixExpression(left, right object.Object) object.Object {
+    switch left.Type() {
+    case object.INTEGER_OBJ:
+        leftVal := left.(*object.Integer).Value
+        switch right.Type() {
+        case object.INTEGER_OBJ:
+            rightVal := right.(*object.Integer).Value
+            return &object.Integer{Value: leftVal * rightVal}
+        case object.FLOAT_OBJ:
+            rightVal := right.(*object.Float).Value
+            return &object.Float{Value: float64(leftVal) * rightVal}
+        }
+    case object.FLOAT_OBJ:
+        leftVal := left.(*object.Float).Value
+        switch right.Type() {
+        case object.INTEGER_OBJ:
+            rightVal := right.(*object.Integer).Value
+            return &object.Float{Value: leftVal * float64(rightVal)}
+        case object.FLOAT_OBJ:
+            rightVal := right.(*object.Float).Value
+            return &object.Float{Value: leftVal * rightVal}
+        }
+    }
+    // TODO: Figure out error handling
+    return NULL
+}
+
+func evalSubtractInfixExpression(left, right object.Object) object.Object {
+    switch left.Type() {
+    case object.INTEGER_OBJ:
+        leftVal := left.(*object.Integer).Value
+        switch right.Type() {
+        case object.INTEGER_OBJ:
+            rightVal := right.(*object.Integer).Value
+            return &object.Integer{Value: leftVal - rightVal}
+        case object.FLOAT_OBJ:
+            rightVal := right.(*object.Float).Value
+            return &object.Float{Value: float64(leftVal) - rightVal}
+        }
+    case object.FLOAT_OBJ:
+        leftVal := left.(*object.Float).Value
+        switch right.Type() {
+        case object.INTEGER_OBJ:
+            rightVal := right.(*object.Integer).Value
+            return &object.Float{Value: leftVal - float64(rightVal)}
+        case object.FLOAT_OBJ:
+            rightVal := right.(*object.Float).Value
+            return &object.Float{Value: leftVal - rightVal}
+        }
+    }
+    // TODO: Figure out error handling
+    return NULL
+}
+
+func evalDivideInfixExpression(left, right object.Object) object.Object {
+    switch left.Type() {
+    case object.INTEGER_OBJ:
+        leftVal := left.(*object.Integer).Value
+        switch right.Type() {
+        case object.INTEGER_OBJ:
+            rightVal := right.(*object.Integer).Value
+            return &object.Integer{Value: leftVal / rightVal}
+        case object.FLOAT_OBJ:
+            rightVal := right.(*object.Float).Value
+            return &object.Float{Value: float64(leftVal) / rightVal}
+        }
+    case object.FLOAT_OBJ:
+        leftVal := left.(*object.Float).Value
+        switch right.Type() {
+        case object.INTEGER_OBJ:
+            rightVal := right.(*object.Integer).Value
+            return &object.Float{Value: leftVal / float64(rightVal)}
+        case object.FLOAT_OBJ:
+            rightVal := right.(*object.Float).Value
+            return &object.Float{Value: leftVal / rightVal}
+        }
+    }
+    // TODO: Figure out error handling
+    return NULL
+}
+
+func evalLTInfixExpression(left, right object.Object) object.Object {
+    switch left.Type() {
+    case object.INTEGER_OBJ:
+        leftVal := left.(*object.Integer).Value
+        switch right.Type() {
+        case object.INTEGER_OBJ:
+            rightVal := right.(*object.Integer).Value
+            return nativeBoolToBooleanObject(leftVal < rightVal)
+        case object.FLOAT_OBJ:
+            rightVal := right.(*object.Float).Value
+            return nativeBoolToBooleanObject(float64(leftVal) < rightVal)
+        }
+    case object.FLOAT_OBJ:
+        leftVal := left.(*object.Float).Value
+        switch right.Type() {
+        case object.INTEGER_OBJ:
+            rightVal := right.(*object.Integer).Value
+            return nativeBoolToBooleanObject(leftVal < float64(rightVal))
+        case object.FLOAT_OBJ:
+            rightVal := right.(*object.Float).Value
+            return nativeBoolToBooleanObject(leftVal < rightVal)
+        }
+    }
+    // TODO: Figure out error handling
+    return NULL
+}
+
+func evalGTInfixExpression(left, right object.Object) object.Object {
+    switch left.Type() {
+    case object.INTEGER_OBJ:
+        leftVal := left.(*object.Integer).Value
+        switch right.Type() {
+        case object.INTEGER_OBJ:
+            rightVal := right.(*object.Integer).Value
+            return nativeBoolToBooleanObject(leftVal > rightVal)
+        case object.FLOAT_OBJ:
+            rightVal := right.(*object.Float).Value
+            return nativeBoolToBooleanObject(float64(leftVal) > rightVal)
+        }
+    case object.FLOAT_OBJ:
+        leftVal := left.(*object.Float).Value
+        switch right.Type() {
+        case object.INTEGER_OBJ:
+            rightVal := right.(*object.Integer).Value
+            return nativeBoolToBooleanObject(leftVal > float64(rightVal))
+        case object.FLOAT_OBJ:
+            rightVal := right.(*object.Float).Value
+            return nativeBoolToBooleanObject(leftVal > rightVal)
+        }
+    }
+    // TODO: Figure out error handling
+    return NULL
+}
+
+func evalEqualsInfixExpression(left, right object.Object) object.Object {
+    switch left.Type() {
+    case object.INTEGER_OBJ:
+        leftVal := left.(*object.Integer).Value
+        switch right.Type() {
+        case object.INTEGER_OBJ:
+            rightVal := right.(*object.Integer).Value
+            return nativeBoolToBooleanObject(leftVal == rightVal)
+        case object.FLOAT_OBJ:
+            rightVal := right.(*object.Float).Value
+            return nativeBoolToBooleanObject(float64(leftVal) == rightVal)
+        }
+    case object.FLOAT_OBJ:
+        leftVal := left.(*object.Float).Value
+        switch right.Type() {
+        case object.INTEGER_OBJ:
+            rightVal := right.(*object.Integer).Value
+            return nativeBoolToBooleanObject(leftVal == float64(rightVal))
+        case object.FLOAT_OBJ:
+            rightVal := right.(*object.Float).Value
+            return nativeBoolToBooleanObject(leftVal == rightVal)
+        }
+    }
+    if left.Type() == object.BOOLEAN_OBJ && right.Type() == object.BOOLEAN_OBJ {
+        return nativeBoolToBooleanObject(left == right)
+    }
+    // TODO: Figure out error handling
+    return NULL
+}
+
+func evalNotEqualsInfixExpression(left, right object.Object) object.Object {
+    switch left.Type() {
+    case object.INTEGER_OBJ:
+        leftVal := left.(*object.Integer).Value
+        switch right.Type() {
+        case object.INTEGER_OBJ:
+            rightVal := right.(*object.Integer).Value
+            return nativeBoolToBooleanObject(leftVal != rightVal)
+        case object.FLOAT_OBJ:
+            rightVal := right.(*object.Float).Value
+            return nativeBoolToBooleanObject(float64(leftVal) != rightVal)
+        }
+    case object.FLOAT_OBJ:
+        leftVal := left.(*object.Float).Value
+        switch right.Type() {
+        case object.INTEGER_OBJ:
+            rightVal := right.(*object.Integer).Value
+            return nativeBoolToBooleanObject(leftVal != float64(rightVal))
+        case object.FLOAT_OBJ:
+            rightVal := right.(*object.Float).Value
+            return nativeBoolToBooleanObject(leftVal != rightVal)
+        }
+    }
+    if left.Type() == object.BOOLEAN_OBJ && right.Type() == object.BOOLEAN_OBJ {
+        return nativeBoolToBooleanObject(left != right)
+    }
+    // TODO: Figure out error handling
+    return NULL
+}
+
--- src/luederlang/evaluator/evaluator_test.go
+++ src/luederlang/evaluator/evaluator_test.go
@@ -35,6 +35,18 @@ func TestEvalIntegerExpression(t *testing.T) {
     } {
         {"5", 5},
         {"10", 10},
+        {"-5", -5},
+		{"5 + 5 + 5 + 5 - 10", 10},
+		{"2 * 2 * 2 * 2 * 2", 32},
+		{"-50 + 100 + -50", 0},
+		{"5 * 2 + 10", 20},
+		{"5 + 2 * 10", 25},
+		{"20 + 2 * -10", 0},
+		{"50 / 2 * 2 + 10", 60},
+		{"2 * (5 + 10)", 30},
+		{"3 * 3 * 3 + 10", 37},
+		{"3 * (3 * 3) + 10", 37},
+		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
     }
     
     for _, tt := range tests {
@@ -56,7 +68,6 @@ func testFloatObject(t *testing.T, obj object.Object, expected float64) bool {
     return true
 }
 
-
 func TestEvalFloatExpression(t *testing.T) {
     tests := []struct {
         input string
@@ -65,10 +76,87 @@ func TestEvalFloatExpression(t *testing.T) {
         {"5.4", 5.4},
         {"10.051", 10.051},
         {"69.420", 69.420},
+        {"-69.420", -69.420},
+		{"3.5 * (3 * 3) + 10", 41.5},
+		{"(5 + 10 * 2 + 15 / 3) * 2.5 + -10", 65},
     }
     
     for _, tt := range tests {
         evaluated := testEval(tt.input)
         testFloatObject(t, evaluated, tt.expected)
     }
+}
+
+func testNullObject(t *testing.T, obj object.Object) bool {
+    _, ok := obj.(*object.Null)
+    if !ok {
+        t.Errorf("object is not a Null. got=%T (+%v)", obj, obj)
+        return false
+    }
+    return true
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool, test string) bool {
+    res, ok := obj.(*object.Boolean)
+    if !ok {
+        t.Errorf("%s | object is not a Boolean. got=%T (+%v)", test, obj, obj)
+        return false
+    }
+    if res.Value != expected {
+        t.Errorf("object has wrong value. got=%t. want=%t", res.Value, expected)
+        return false
+    }
+    return true
+}
+
+func TestBangOperator(t *testing.T) {
+    test := []struct {
+        input string
+        expected bool
+    } {
+        {"!true", false},
+        {"!false", true},
+        {"!!true", true},
+        {"!!!true", false},
+    }
+
+    for _, tt := range test {
+        evaluated := testEval(tt.input)
+        testBooleanObject(t, evaluated, tt.expected, tt.input)
+    }
+    
+    evaluated := testEval("!x")
+    testNullObject(t, evaluated)
+}
+
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+		{"1 < 2", true},
+		{"1 > 2", false},
+		{"1 < 1", false},
+		{"1 > 1", false},
+		{"1 == 1", true},
+		{"1 != 1", false},
+		{"1 == 2", false},
+		{"1 != 2", true},
+		{"true == true", true},
+		{"false == false", true},
+		{"true == false", false},
+		{"true != false", true},
+		{"false != true", true},
+		{"(1 < 2) == true", true},
+		{"(1 < 2) == false", false},
+		{"(1 > 2) == true", false},
+		{"(1 > 2) == false", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected, tt.input)
+	}
 }
